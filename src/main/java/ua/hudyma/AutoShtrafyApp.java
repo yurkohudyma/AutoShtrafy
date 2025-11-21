343ea581a4ba8b6a44f25ded6c1e1720380a03a7
yurkohudyma <hudyma@gmail.com>
2025-11-21 12:30:10 +0200

    init commit

---
+++ src/main/java/ua/hudyma/AutoShtrafyApp.java
@@ -0,0 +1,11 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShtrafyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoShtrafyApp.class, args);
+    }
+}
